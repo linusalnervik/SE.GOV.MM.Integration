c303d6d45ff40999000b4b581b30b700f37b166c
linus.alnervik <linus.alnervik@intraservice.goteborg.se>
2020-05-29 23:52:56 +0200

    Adding missing method to interface

--- SE.GOV.MM.Integration/SE.GOV.MM.Integration.Core/Interface/IMessageService.cs
+++ SE.GOV.MM.Integration/SE.GOV.MM.Integration.Core/Interface/IMessageService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml;
 
 namespace SE.GOV.MM.Integration.Infrastructure.Services
 {
@@ -7,5 +8,6 @@ namespace SE.GOV.MM.Integration.Infrastructure.Services
         DeliveryResult distributeSecure(SignedDelivery2 signedDelivery2, string endpointAdress, string signingCertificateSubjectName);
         DeliveryResult distributeSecure(SignedDelivery2 signedDelivery2, string endpointAdress, string certificateUrl, string certificatePassword);
         DeliveryResult distributeSecure(SignedDelivery2 signedDelivery2, string endpointAdress, X509Certificate2 x509Certificate2);
+        bool IsValidSignature(XmlDocument xmlDocument);
     }
 }
